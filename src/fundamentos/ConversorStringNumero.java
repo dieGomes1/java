d2befe5d79ca3996f91b5598cc7d5831a377a898
Diego Fernandes <dfernandesgomes@gmail.com>
2019-04-06 15:22:20 -0300

    criação repositorio de estudo Java

---
+++ src/fundamentos/ConversorStringNumero.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+import javax.swing.*;
+
+public class ConversorStringNumero {
+    public static void main(String[]args){
+        String resposta1 = JOptionPane.showInputDialog("Qual a primeira nota");
+        String resposta2 = JOptionPane.showInputDialog("Qual a segunda nota");
+
+
+        System.out.println(resposta1 + resposta2);
+
+        double nota1 = Double.parseDouble(resposta1);
+        double nota2 = Double.parseDouble(resposta2);
+
+        System.out.println(nota1 + nota2);
+
+
+    }
+}
